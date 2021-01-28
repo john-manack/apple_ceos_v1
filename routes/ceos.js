0a1c22fedd1552be27c2be57ebb605b12d9ebf3a
John Manack <jmnck28@gmail.com>
2021-01-28 13:02:43 -0500

    adds html to templates

--- routes/ceos.js
+++ routes/ceos.js
@@ -3,14 +3,31 @@
 const express = require('express'),
     router = express.Router();
 
+const ceosModel = require('../db')
+
 router.get('/', (req, res) => {
-    res.send(`<h1>CEO List Page</h1>`)
+    res.render('template', {
+        locals: {
+            title: "List of Apple CEOs",
+            data: ceosModel,
+        },
+        partials: {
+            body: 'partials/ceo-list',
+        }
+    });
 });
 
 router.get('/:slug', (req, res) => {
     const { slug } = req.params; // DECONSTRUCTING
     console.log("Slug is: ", slug);
-    res.send(`<h1>Single CEO Page</h1>`);
+    res.render('template', {
+        locals: {
+            title: 'An Apple CEO',
+        },
+        partials: {
+            body: 'partials/ceo-details',
+        }
+    })
 })
 
 module.exports = router;
